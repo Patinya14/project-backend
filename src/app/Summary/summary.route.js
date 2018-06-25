2394102c0d3a26e0ea1c33cb34ff2e8d1b83eecd
Patinya14 <57160342@go.buu.ac.th>
2018-06-25 13:50:49 +0700

    fix summary

--- src/app/Summary/summary.route.js
+++ src/app/Summary/summary.route.js
@@ -2,13 +2,27 @@ const express = require('express') // create constant value for use express liba
 const router = express.Router() // create constant value for use Router by express libary
 const lib = require('../Library/pdfmake/pdf-summary');
 const service = require('./summary.service');
+
+let func = {}
+
 router.get('/summary', (req, res) => {
-    service.call().then((summary) => {  
-        res.json(summary) // response data with JSON.
+    service.call().then((summary) => {
+        (async () => {
+            let data = []
+            for (let i = 0; i < summary.length; i++) {
+                let status = await service.find(summary[i].personId._id).then(cursor => {
+                    if (cursor.length > 1) return 'ผู้ป่วยรายเก่า';
+                    else  return 'ผู้ป่วยรายใหม่';
+                })
+                data.push(func.pushSummary(summary[i], status))
+            }
+            res.json(data)
+        })();
     });
 });
+
 router.get('/summary/:id', (req, res) => {
-    service.find(req.params.id).then((summary) => {   
+    service.find(req.params.id).then((summary) => {
         res.json(summary) // response data with JSON.
     });
 });
@@ -16,11 +30,11 @@ router.get('/summary/getpdf/:id', (req, res) => {
     service.call().then((summary) => {
         lib.document(summary);
         res.json({})
-        
+
     });
 });
 router.post('/summary', (req, res) => {
-    service.create(req.body).then((summary) => {   
+    service.create(req.body).then((summary) => {
         res.json(summary) // response data with JSON.
     });
 });
@@ -35,4 +49,29 @@ router.delete('/summary/:id', (req, res) => {
     });
 });
 
+func.pushSummary = (summary, status) => {
+    return {
+        treatment: summary.treatment,
+        countDrugs: summary.countDrugs,
+        personId: summary.personId,
+        disease: summary.disease,
+        treater: summary.treater,
+        officer: summary.officer,
+        date: summary.date,
+        time: summary.time,
+        // charge: func.calculateCharge(summary.treatment, summary.statusTime),
+        status: status,
+        statusTime: summary.statusTime
+    }
+}
+
+func.calculateCharge = (treatment, statusTime) => {
+    if ( statusTime !== undefined && statusTime !== '') {
+        treatment.forEach(treat => {
+            if(statusTime === 'ในเวลา') {
+                Number(treat.treatInTime)
+            }
+        });
+    }
+}
 module.exports = router
