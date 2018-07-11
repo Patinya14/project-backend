171f253d41307d4de1c28398b0668547f247e2c4
Patinya14 <57160342@go.buu.ac.th>
2018-07-12 02:54:34 +0700

    m

--- src/app/User/user.router.js
+++ src/app/User/user.router.js
@@ -3,9 +3,12 @@ const router = express.Router() // create constant value for use Router by expre
 const service = require('./user.service');
 
 router.post('/login', (req, res) => {
-    service.create(req.body).then((user) => { // req.body is all-subject data at user new entered.  
-        res.status(200).send(true)
-    });
+    service.login(req.body).then((user) => { // req.body is all-subject data at user new entered.  
+        res.status(200).send({ access_token: true })
+    })
+        .catch(() => {
+            res.status(401).json({ error: 'Unauthorized' });
+        });
 });
 
 module.exports = router
