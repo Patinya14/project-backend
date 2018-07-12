5be9d557fd63130d35d3cb0ed345a678e5ab6f80
Woawut Niamsiri <niamsiri.em@gmail.com>
2018-07-12 09:57:57 +0700

    login user

--- src/app/User/user.router.js
+++ src/app/User/user.router.js
@@ -3,8 +3,9 @@ const router = express.Router() // create constant value for use Router by expre
 const service = require('./user.service');
 
 router.post('/login', (req, res) => {
-    service.login(req.body).then((user) => { // req.body is all-subject data at user new entered.  
-        res.status(200).send({ access_token: true })
+    service.login(req.body).then((user) => { // req.body is all-subject data at user new entered.
+        if (user._id !== undefined) res.status(200).send({ access_token: true })
+        else res.status(200).send({ access_token: false })
     })
         .catch(() => {
             res.status(401).json({ error: 'Unauthorized' });
